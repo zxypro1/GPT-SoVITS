25a970225ca100b18d1dd9c45dc4746b9410db2f
Zone Tome <1018995004@qq.com>
2024-11-19 16:55:25 +0000

    fix path

--- tools/uvr5/vr.py
+++ tools/uvr5/vr.py
@@ -7,11 +7,11 @@ import librosa
 import numpy as np
 import soundfile as sf
 import torch
-from lib.lib_v5 import nets_61968KB as Nets
-from lib.lib_v5 import spec_utils
-from lib.lib_v5.model_param_init import ModelParameters
-from lib.lib_v5.nets_new import CascadedNet
-from lib.utils import inference
+from tools.uvr5.lib.lib_v5 import nets_61968KB as Nets
+from tools.uvr5.lib.lib_v5 import spec_utils
+from tools.uvr5.lib.lib_v5.model_param_init import ModelParameters
+from tools.uvr5.lib.lib_v5.nets_new import CascadedNet
+from tools.uvr5.lib.utils import inference
 
 
 class AudioPre:
--- train_api.py
+++ train_api.py
@@ -30,7 +30,6 @@ import yaml
 from tools.asr.config import asr_dict
 import torch
 from tools.uvr5.mdxnet import MDXNetDereverb
-from tools.uvr5.vr import AudioPre, AudioPreDeEcho
 from tools.uvr5.bsroformer import BsRoformer_Loader
 from tools.task_manager import TaskManager, TaskStatus
 import uuid
@@ -38,6 +37,7 @@ import ffmpeg
 from contextlib import asynccontextmanager
 import asyncio
 import requests
+from tools.uvr5.vr import AudioPre, AudioPreDeEcho
 
 # print(sys.path)
 i18n = I18nAuto()

351e4fefba653d3dccf93ba12f81405311de54a2
Zone Tome <1018995004@qq.com>
2024-11-20 07:20:01 +0000

    fix api

--- train_api.py
+++ train_api.py
@@ -106,86 +106,82 @@ for root in SoVITS_weight_root+GPT_weight_root:
     os.makedirs(root,exist_ok=True)
 def uvr(model_name, inp_root, save_root_vocal, paths, save_root_ins, agg, format0, device, is_half):
     infos = []
-    try:
-        inp_root = clean_path(inp_root)
-        save_root_vocal = clean_path(save_root_vocal)
-        save_root_ins = clean_path(save_root_ins)
-        is_hp3 = "HP3" in model_name
-        if model_name == "onnx_dereverb_By_FoxJoy":
-            pre_fun = MDXNetDereverb(15)
-        elif model_name == "Bs_Roformer" or "bs_roformer" in model_name.lower():
-            func = BsRoformer_Loader
-            pre_fun = func(
-                model_path = os.path.join(weight_uvr5_root, model_name + ".ckpt"),
-                device = device,
-                is_half=is_half
-            )
-        else:
-            func = AudioPre if "DeEcho" not in model_name else AudioPreDeEcho
-            pre_fun = func(
-                agg=int(agg),
-                model_path=os.path.join(weight_uvr5_root, model_name + ".pth"),
-                device=device,
-                is_half=is_half,
-            )
-        if inp_root != "":
-            paths = [os.path.join(inp_root, name) for name in os.listdir(inp_root)]
-        else:
-            paths = [path.name for path in paths]
-        for path in paths:
-            inp_path = os.path.join(inp_root, path)
-            if(os.path.isfile(inp_path)==False):continue
-            need_reformat = 1
-            done = 0
-            try:
-                info = ffmpeg.probe(inp_path, cmd="ffprobe")
-                if (
-                    info["streams"][0]["channels"] == 2
-                    and info["streams"][0]["sample_rate"] == "44100"
-                ):
-                    need_reformat = 0
-                    pre_fun._path_audio_(
-                        inp_path, save_root_ins, save_root_vocal, format0,is_hp3
-                    )
-                    done = 1
-            except:
-                need_reformat = 1
-                traceback.print_exc()
-            if need_reformat == 1:
-                os.makedirs(os.path.join(now_dir, "TEMP"), exist_ok=True)
-                tmp_path = "%s/%s.reformatted.wav" % (
-                    os.path.join(now_dir, "TEMP"),
-                    os.path.basename(inp_path),
-                )
-                os.system(
-                    f'ffmpeg -i "{inp_path}" -vn -acodec pcm_s16le -ac 2 -ar 44100 "{tmp_path}" -y'
-                )
-                inp_path = tmp_path
-            try:
-                if done == 0:
-                    pre_fun._path_audio_(
-                        inp_path, save_root_ins, save_root_vocal, format0,is_hp3
-                    )
-                infos.append("%s->Success" % (os.path.basename(inp_path)))
-            except:
-                infos.append(
-                    "%s->%s" % (os.path.basename(inp_path), traceback.format_exc())
-                )
-    except:
-        infos.append(traceback.format_exc())
-    finally:
+    inp_root = clean_path(inp_root)
+    save_root_vocal = clean_path(save_root_vocal)
+    save_root_ins = clean_path(save_root_ins)
+    is_hp3 = "HP3" in model_name
+    if model_name == "onnx_dereverb_By_FoxJoy":
+        pre_fun = MDXNetDereverb(15)
+    elif model_name == "Bs_Roformer" or "bs_roformer" in model_name.lower():
+        func = BsRoformer_Loader
+        pre_fun = func(
+            model_path = os.path.join(weight_uvr5_root, model_name + ".ckpt"),
+            device = device,
+            is_half=is_half
+        )
+    else:
+        func = AudioPre if "DeEcho" not in model_name else AudioPreDeEcho
+        pre_fun = func(
+            agg=int(agg),
+            model_path=os.path.join(weight_uvr5_root, model_name + ".pth"),
+            device=device,
+            is_half=is_half,
+        )
+    if inp_root != "":
+        paths = [os.path.join(inp_root, name) for name in os.listdir(inp_root)]
+    else:
+        paths = [path.name for path in paths]
+    for path in paths:
+        inp_path = os.path.join(inp_root, path)
+        if(os.path.isfile(inp_path)==False):continue
+        need_reformat = 1
+        done = 0
         try:
-            if model_name == "onnx_dereverb_By_FoxJoy":
-                del pre_fun.pred.model
-                del pre_fun.pred.model_
-            else:
-                del pre_fun.model
-                del pre_fun
+            info = ffmpeg.probe(inp_path, cmd="ffprobe")
+            if (
+                info["streams"][0]["channels"] == 2
+                and info["streams"][0]["sample_rate"] == "44100"
+            ):
+                need_reformat = 0
+                pre_fun._path_audio_(
+                    inp_path, save_root_ins, save_root_vocal, format0,is_hp3
+                )
+                done = 1
         except:
+            need_reformat = 1
             traceback.print_exc()
-        print("clean_empty_cache")
-        if torch.cuda.is_available():
-            torch.cuda.empty_cache()
+        if need_reformat == 1:
+            os.makedirs(os.path.join(now_dir, "TEMP"), exist_ok=True)
+            tmp_path = "%s/%s.reformatted.wav" % (
+                os.path.join(now_dir, "TEMP"),
+                os.path.basename(inp_path),
+            )
+            os.system(
+                f'ffmpeg -i "{inp_path}" -vn -acodec pcm_s16le -ac 2 -ar 44100 "{tmp_path}" -y'
+            )
+            inp_path = tmp_path
+        # try:
+        if done == 0:
+            pre_fun._path_audio_(
+                inp_path, save_root_ins, save_root_vocal, format0,is_hp3
+            )
+        infos.append("%s->Success" % (os.path.basename(inp_path)))
+        # except:
+        #     infos.append(
+        #         "%s->%s" % (os.path.basename(inp_path), traceback.format_exc())
+        #     )
+    try:
+        if model_name == "onnx_dereverb_By_FoxJoy":
+            del pre_fun.pred.model
+            del pre_fun.pred.model_
+        else:
+            del pre_fun.model
+            del pre_fun
+    except:
+        traceback.print_exc()
+    print("clean_empty_cache")
+    if torch.cuda.is_available():
+        torch.cuda.empty_cache()
     return "\n".join(infos)
 
 class UVRRequest(BaseModel):
